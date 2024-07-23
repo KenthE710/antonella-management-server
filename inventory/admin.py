02fb5c05ec22002d7388e1fd39828ce4f55c3677
KenthE710 <kenencalada@gmail.com>
2024-07-23 16:36:28 -0500

    cambio de la app de inventory a submodulo

--- inventory/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Producto, ProductoImg, ProductoMarca, ProductoTipo, Lote
-
-admin.site.register(Producto)
-admin.site.register(ProductoTipo)
-admin.site.register(ProductoMarca)
-admin.site.register(ProductoImg)
-admin.site.register(Lote)
