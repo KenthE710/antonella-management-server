30055e686d91f928a3a932ac8a376a0a9f39a668
KenthE710 <kenencalada@gmail.com>
2024-07-23 18:29:26 -0500

    Separación de la app core como submodulo

--- core/pagination.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.pagination import CursorPagination
-
-class createdDateCursorPagination(CursorPagination):
-    page_size = 10
-    ordering = 'created_at'
--- core/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-
-class PaginacionSerializer(serializers.Serializer):
-    page = serializers.IntegerField(min_value=1)
-    page_size = serializers.IntegerField(min_value=1)
